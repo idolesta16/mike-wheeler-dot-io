9c4d36a4465717705cc299842cc41cf8b5fa83de
Michael Wheeler <wheeler.m@husky.neu.edu>
2019-09-20 21:51:48 -0400

    Debug text updates with window resize.

--- debug.js
+++ debug.js
@@ -3,5 +3,12 @@ Debug utility JavaScript.
 
 Written by Michael Wheeler.
  */
-let debugEl = document.getElementById("debug");
-debugEl.innerText = `Width: ${window.innerWidth}px; Height: ${window.innerHeight}px`;
+
+function printDebug(event) {
+    let debugEl = document.getElementById("debug");
+    debugEl.innerText = `Width: ${window.innerWidth}px; Height: ${window.innerHeight}px`;
+}
+
+printDebug();
+window.onresize = printDebug;
+
